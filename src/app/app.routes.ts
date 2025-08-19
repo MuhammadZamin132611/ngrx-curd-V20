e4dd50b5f0f602218ff70305800d28f115c19e22
Zamin <Muhammad.Zamin@pratian.com>
2025-08-19 13:41:53 +0530

    Adding home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,13 @@ import { EditEmployee } from './employee/component/edit-employee/edit-employee';
 import { Login } from './employee/component/login/login';
 import { Signup } from './employee/component/signup/signup';
 import { authGuard, roleGuard } from './employee/gurd/auth-guard';
+import { EmployeeHome } from './employee/component/employee-home/employee-home';
 
 export const routes: Routes = [
+    { path: 'employee', component: EmployeeHome, title: 'Employee', },
     { path: 'login', component: Login, title: 'All Employee', canActivate: [authGuard] },
     { path: 'signup', component: Signup, title: 'All Employee', canActivate: [authGuard] },
-    { path: 'employee', component: ViewEmployee, title: 'All Employee', canActivate: [authGuard], },
+    { path: 'view-employee', component: ViewEmployee, title: 'All Employee', canActivate: [authGuard], },
     { path: 'add-employee', component: AddEmployee, title: 'Add Employee', canActivate: [authGuard, roleGuard], data: { isAdmin: [true] } },
     { path: 'edti-employee/:id', component: EditEmployee, title: 'Edit Employee', canActivate: [authGuard, roleGuard], data: { isAdmin: [true] } },
     { path: '', redirectTo: '/employee', pathMatch: 'full', },
---
+++ src/app/employee/component/employee-home/employee-home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-home',
+  imports: [],
+  templateUrl: './employee-home.html',
+  styleUrl: './employee-home.scss'
+})
+export class EmployeeHome {
+
+}
