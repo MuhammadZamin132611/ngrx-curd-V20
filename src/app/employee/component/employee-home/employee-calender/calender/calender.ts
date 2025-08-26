31d0ae5b86806e9dc19bffdf5cdfbd5a3d24b255
Zamin <Muhammad.Zamin@pratian.com>
2025-08-26 14:29:24 +0530

    update my code

--- src/app/employee/component/employee-home/employee-calender/calender/calender.ts
+++ src/app/employee/component/employee-home/employee-calender/calender/calender.ts
@@ -1,11 +1,20 @@
+import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../../../../Material.module';
 
 @Component({
   selector: 'app-calender',
-  imports: [],
+  imports: [MaterialModule, NgStyle],
   templateUrl: './calender.html',
   styleUrl: './calender.scss'
 })
 export class Calender {
+  times = ['9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM'];
+  days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
 
+  events = [
+    { title: 'Team Meeting', day: 'Monday', startIndex: 1, color: '#2563eb' },   // 10 AM
+    { title: 'Client Call', day: 'Wednesday', startIndex: 3, color: '#facc15' }, // 12 PM
+    { title: 'Workshop', day: 'Thursday', startIndex: 4, color: '#22c55e' }     // 1 PM
+  ];
 }
